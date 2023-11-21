c8813c6f448e0bd777b3fa59fd12a7191b85411a
Nimi <Nimi.Flaisher@Gmail.com>
2023-11-21 12:38:26 +0200

    Add scroll animation to details image

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./MovieDetails.css";
 
 function MovieDetails({ movie, onFavoriteToggle, favorites }) {
+  const [animationKey, setAnimationKey] = useState(0);
+
+  useEffect(() => {
+    if (movie) {
+      setAnimationKey((prevKey) => prevKey + 1);
+    }
+  }, [movie]);
+
   if (!movie) return null;
 
   return (
     <div className="movie-details">
       <div className="movie-image-container">
         <img
+          key={animationKey}
           src={movie.image_url}
           alt={movie.title}
           className="movie-details-image"
